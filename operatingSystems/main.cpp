fe0d31a97b79b67f737e87c70a4aa5739652a064
Anthony Stanfel <anthony@Anthonys-MacBook-Pro.local>
2015-08-24 14:17:20 -0600

    Initial Commit

---
+++ operatingSystems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  operatingSystems
+//
+//  Created by Anthony Stanfel  on 8/24/15.
+//  Copyright (c) 2015 Anthony Stanfel . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
